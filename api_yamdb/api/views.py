93c1a4d1a496a3505d900455800fbe70598affe7
Денис <eltimccc@yandex.ru>
2022-10-26 08:38:38 +0300

    349 views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -346,9 +346,9 @@ class ReviewViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(
             review, data=self.request.data, partial=True
         )
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        # if serializer.is_valid(raise_exception=True):
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk, title_id):
         if not Title.objects.filter(id=title_id).exists():
